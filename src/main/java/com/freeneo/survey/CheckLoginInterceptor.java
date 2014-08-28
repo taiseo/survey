d5f86f7075c2c1c800ac4c31e05fec8c99a7183d
mytory <mytory@gmail.com>
2014-08-29 07:18:05 +0900

    로컬에선 로그인 필요 없게.

--- src/main/java/com/freeneo/survey/CheckLoginInterceptor.java
+++ src/main/java/com/freeneo/survey/CheckLoginInterceptor.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.freeneo.survey.domain.User;
+
 public class CheckLoginInterceptor extends HandlerInterceptorAdapter {
 	
 	private static Logger logger = LoggerFactory.getLogger(CheckLoginInterceptor.class);
@@ -16,8 +18,14 @@ public class CheckLoginInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response,
 			Object handler) throws Exception {
+
+		HttpSession session = request.getSession();
 		
-		logger.debug(request.getRequestURI());
+		// 로컬에서 개발중이면 로그인 검사 패스
+		if(request.getRemoteAddr().equals("127.0.0.1") || request.getRemoteAddr().equals("0:0:0:0:0:0:0:1")){
+			session.setAttribute("user", new User(1L, "admin", "", "최고관리자", "최고관리자", "", "email", "admin"));
+			return true;
+		}
 		
 		if((request.getContextPath() + "/login").equals(request.getRequestURI())) {
 			return true;
@@ -26,8 +34,6 @@ public class CheckLoginInterceptor extends HandlerInterceptorAdapter {
 		if((request.getContextPath() + "/").equals(request.getRequestURI())) {
 			return true;
 		};
-		
-		HttpSession session = request.getSession();
 		
 		if(session.getAttribute("user") == null){
 			response.sendRedirect(request.getContextPath() + "/login");
