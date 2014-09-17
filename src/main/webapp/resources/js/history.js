feb738799ebd853407ea386f04e9c2eaf5fe9833
mytory <mytory@gmail.com>
2014-09-17 11:16:10 +0900

    운영 및 통계 내역 지사별 검색 셀렉트박스에 전체 검색 항목 추가

--- src/main/webapp/resources/js/history.js
+++ src/main/webapp/resources/js/history.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	bind_name_by_part();
 	print_bonbu_select_el();
+	print_branch_select_el();
 	bind_branch_select_el();
 });
 
@@ -13,7 +14,7 @@ survey.branches = {
 	'전남' : ['광주지사', '순천광양여수지사', '나주지사', '담양지사', '곡성지사', '구례지사', '고흥지사', '보성지사', '화순지사', '장흥지사', '강진완도지사', '해남지사', '영암지사', '무안신안지사', '함평지사', '영광지사', '장성지사', '진도지사'],
 	'경북' : ['포항을릉지사', '경주지사', '안동지사', '구미김천지사', '영주봉화지사', '영천지사', '상주지사', '문경지사', '경산청도지사', '의성군위지사', '청송영양지사', '영덕울진지사', '고령지사', '성주지사', '칠곡지사', '예천지사', '달성지사'], 
 	'경남' : ['김해양산지사', '고성통영거제지사', '울산지사', '진주산청지사', '의령지사', '함안지사', '창녕지사', '밀양지사', '창원지사', '사천지사', '거창함양지사', '합천지사', '하동남해지사'],
-	'제주' : []
+	'제주' : ['지사 없음']
 };
 
 function bind_name_by_part(){
@@ -56,6 +57,7 @@ function search_by_user(){
 }
 
 function print_bonbu_select_el(){
+	$('#bonbu').append('<option value="">전체</option>');
 	_.each(survey.branches, function(branch, index, list){
 		var option = document.createElement("option");
 		$('#bonbu').append('<option>' + index + '</option>');
@@ -63,11 +65,23 @@ function print_bonbu_select_el(){
 }
 
 function bind_branch_select_el(){
-	$('.js-search-by-branch').on('change', '#bonbu', function(){
-		var bonbu = $('#bonbu').val();
-		$('[name=branch]').html('');
+	$('.js-search-by-branch').on('change', '#bonbu', print_branch_select_el);
+}
+
+function print_branch_select_el(){
+	$('[name=branch]')
+		.html('')
+		.append('<option value="">전체</option>');
+	var bonbu = $('#bonbu').val();
+	if(bonbu == ''){
+		_.each(survey.branches, function(el){
+			_.each(el, function(branch){
+				$('[name=branch]').append('<option>' + branch + '</option>');
+			});
+		});
+	}else{
 		_.each(survey.branches[bonbu], function(branch){
 			$('[name=branch]').append('<option>' + branch + '</option>');
 		});
-	});
+	}
 }
